3363a7eedad0e1d49299e825c442a00f47bd6890
Joko Santoso <joko74479@gmail.com>
2026-06-22 00:32:03 +0700

    test hook

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,23 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+import betterTailwindcss from 'eslint-plugin-better-tailwindcss';
+import prettier from 'eslint-config-prettier/flat';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+  {
+    extends: [betterTailwindcss.configs.correctness],
+    settings: {
+      'better-tailwindcss': {
+        // path ke file entry CSS yang berisi @import "tailwindcss"
+        entryPoint: 'src/app/globals.css',
+      },
+    },
+  },
+  prettier,
+  globalIgnores(['.next/**', 'out/**', 'build/**', 'next-env.d.ts']),
 ]);
 
 export default eslintConfig;
